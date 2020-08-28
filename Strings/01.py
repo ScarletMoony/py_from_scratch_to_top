8de20238906863dd919929edf2d1f90fbb3cad17
ScarletMoon <alexgameshow1@gmail.com>
2020-08-28 17:24:22 +0300

    added first example of code to file 01.py

---
+++ Strings/01.py
@@ -0,0 +1,12 @@
+x = "S S \nL L \nl l \ns s \n_________ \n"
+
+print(len(x)) #Показывает количество символов в строке x
+print(x.count ('l'))
+print(x.capitalize())
+print(x.upper())
+print(x.lower())
+print(x)
+print(x.islower())
+print(x.isupper())
+print(x.find ('l'))
+print(x.find ('l', 11, 13))
