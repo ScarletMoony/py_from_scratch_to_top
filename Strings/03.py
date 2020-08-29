fbe2d387386f368dbce0f6d9da53efa3df830cbe
ScarletMoon <alexgameshow1@gmail.com>
2020-08-29 18:01:24 +0300

    Added 03.py file

---
+++ Strings/03.py
@@ -0,0 +1,13 @@
+print("Hello {}".format("Alex"))
+
+pi = 3.1415
+
+print("Pi equals {pi:1.2f}".format(pi=pi))
+
+age = input("Age: ")
+name = input("Name: ")
+
+print(f"Hello {name}, you are {age} years old right?") #f означает format
+# с помощю этого можно вставлять переменные в строки и даже писать мини функции
+# как в пятой строке этого файла где указана переменная pi и :1.2f что
+# означает мы хотим чтобы после запятой было лиш 2 символа
