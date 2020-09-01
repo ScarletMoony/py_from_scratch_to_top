1aeda120fa6e32ee60be8f597268c0fdf9dd9449
ScarletMoon <alexgameshow1@gmail.com>
2020-09-01 14:31:20 +0300

    Added all code from lessons about list comprehension to folder (List_comprehension)

---
+++ List_comprehension/01.py
@@ -0,0 +1,13 @@
+x = 'hello'
+
+y = [f for f in x] # list comprehension
+
+print(y)
+
+l = [z for z in range(0, 10)]
+
+print(l)
+
+print([z * z for z in range(0, 5)])
+
+# Можно сказать что это функция в одну строку для списка с использованием цикла
